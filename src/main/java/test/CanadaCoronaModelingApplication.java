c399238896a758df8ad1f94932b9e2a22b2a111b
pspjzy <pspjzy@gmail.com>
2020-04-25 13:37:58 -0400

    finish data retrieve method

--- src/main/java/test/CanadaCoronaModelingApplication.java
+++ src/main/java/test/CanadaCoronaModelingApplication.java
@@ -2,8 +2,10 @@ package test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CanadaCoronaModelingApplication {
 
     public static void main(String[] args) { SpringApplication.run(CanadaCoronaModelingApplication.class,args); }
--- src/main/java/test/service/ccdService.java
+++ src/main/java/test/service/ccdService.java
@@ -1,12 +1,21 @@
 package test.service;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import test.data.entity.DataModel;
 import test.data.repository.DataModelRepo;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Timestamp;
 import java.util.List;
+
 
 @Service
 public class ccdService {
@@ -16,6 +25,48 @@ public class ccdService {
     public ccdService(DataModelRepo dataModelRepo) {
         this.dataModelRepo = dataModelRepo;
     }
+
+    @Scheduled(cron = "0 0 */1 * * *")//start at 28
+    public void autoSetData(){
+        HttpsURLConnection connection;
+        StringBuffer response;
+        try {
+            String APIUrl = "https://corona.lmao.ninja/v2/countries/Canada";
+            URL url = new URL(APIUrl);
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5500);
+            connection.setReadTimeout(5500);
+            connection.setRequestProperty("User-Agent","Mozilla/5.0");
+
+
+
+            int status = connection.getResponseCode();
+
+            if (status==200){
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                Gson gson = new Gson();
+                DataModel data = gson.fromJson(String.valueOf(response),DataModel.class);
+                data.setTime(new Timestamp(System.currentTimeMillis()));
+                dataModelRepo.save(data);
+                in.close();
+            } else {
+                System.out.println("gg");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     public List<DataModel> getDataByDate(Timestamp ts){
         List<DataModel> dataModels = dataModelRepo.findDataModelByTime(new Timestamp(ts.getTime()));
