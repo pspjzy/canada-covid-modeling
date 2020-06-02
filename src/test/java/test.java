21c381ea27d7983c982a1eea239e74e5b2964bef
pspjzy <pspjzy@gmail.com>
2020-06-01 23:15:11 -0400

    Remove the google cloud related file and dependency.

---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import ccm.controller.DataController;
+import ccm.data.repository.DataModelRepo;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class test {
+
+    @Autowired
+    private TestEntityManager em;
+
+
+}
